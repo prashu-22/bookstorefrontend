2abc762355a1152ef8695aa2a39e216353a8d5d8
prashu-22 <139197155+prashu-22@users.noreply.github.com>
2024-05-22 17:33:27 +0530

    Update EditBook.jsx

--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`https://bookstorebackendoooo.onrender.com/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
